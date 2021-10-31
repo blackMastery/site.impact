5c756bd9617bdd2b1157cf25bb72836ed3eef266
blackMastery <black.king1232@gmail.com>
2021-10-31 18:54:03 -0400

    ...

--- pages/services.js
+++ pages/services.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/layout";
-export default function About() {
+export default function Services() {
   return (
     <>
       <Layout title="Services">
